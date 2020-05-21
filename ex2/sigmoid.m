b4ba08146f1418dc174a4a678a91e4660963ae2b
SamarthR26 <55300434+SamarthR26@users.noreply.github.com>
2020-05-21 01:19:32 -0700

    Made Sigmoid Function
    Sigmoid Function for Logistic Regression

--- ex2/sigmoid.m
+++ ex2/sigmoid.m
@@ -1,7 +1,7 @@
 function g = sigmoid(z)
 %SIGMOID Compute sigmoid function
 %   g = SIGMOID(z) computes the sigmoid of z.
-
+% sigmoid function
 % You need to return the following variables correctly 
 g = zeros(size(z));
 
